670b2caa16735e27c091145521483ab9b509a66f
Sudeep Das <datamusing@gmail.com>
2014-03-11 19:23:58 -0700

    pushing version that matches blog

--- bayesSets.py
+++ bayesSets.py
@@ -8,7 +8,8 @@ class bayesSets():
         Basic class for BS modeling
 
         """
-
+        self.alpha = None
+        self.beta = None
         pass
 
     def loadBinarizedDataFromJSON(self, jsonFile):
@@ -33,7 +34,7 @@ class bayesSets():
 
         # Now create the sparse data matrix
         nitems = len(data.keys())
-        nfeatures = len(features_index)
+        nfeatures = len(self.features_index)
 
         self.X = sparse.lil_matrix((nitems, nfeatures))
         self.item_index = {}
@@ -52,7 +53,7 @@ class bayesSets():
     def setHyperParams(self, C=2):
         """
         Set alpha and beta for the multinomial conjugate prior.
-        :param C: hyperparameter coefficient set to 2 by defualt
+        :param C: hyperparameter coefficient set to 2 by default
         """
         self.alpha = C * self.X.mean(0)
         self.beta = C - self.alpha
@@ -63,31 +64,72 @@ class bayesSets():
 
 
     def query(self, queryItemList):
+        assert(self.alpha is not None), 'Hyperparameters not set yet'
+        assert(self.beta is not None), 'Hyperparameters not set yet'
+        assert(self._validate_query(queryItemList)), 'Query not in source items'
+        c, q = self._computeQueryParams(queryItemList)
+        log_scores = c + self.X*q.transpose()
+        log_scores = np.asarray(log_scores.flatten())[0]
+        ranked_ids = np.argsort(log_scores)[::-1]
 
+        return results(queryItemList,self.item_index, ranked_ids, log_scores)
+
+
+
+
+    def _validate_query(self, queryItemList):
+        return set(queryItemList).issubset(set(self.item_index.values()))
+
+    def _computeQueryParams(self, queryItemList):
         N = len(queryItemList)
 
-
-
-
-
-        sum_x = XX[self.item_reverse_index[queryItemList[0]]]
+        sum_x = self.X[self.item_reverse_index[queryItemList[0]]]
         for item in queryItemList[1:]:
-            sum_x = sum_x + XX[self.item_reverse_index[item]]
+            sum_x = sum_x + self.X[self.item_reverse_index[item]]
 
         alpha_tilde = sum_x + self.alpha
         beta_tilde = self.beta + N - sum_x
         log_alpha_tilde = np.log(alpha_tilde)
         log_beta_tilde = np.log(beta_tilde)
-        c = (np.log(alpha+beta) - np.log(alpha+beta+N) + log_beta_tilde - np.log(beta)).sum()
-        #print locals()
-        q = log_alpha_tilde - np.log(alpha) - log_beta_tilde + np.log(beta)
+        c = (self.alpha_plus_beta - np.log(self.alpha + self.beta + N) + log_beta_tilde - self.log_beta).sum()
+        q = log_alpha_tilde - self.log_alpha - log_beta_tilde + self.log_beta
 
-        return c,q
+        return c, q
 
+class results():
+    def __init__(self, queryItemList, item_index, ranked_ids, log_scores):
+        self.queryItemList = queryItemList
+        self.item_index = item_index
+        self.ranked_ids = ranked_ids
+        self.log_scores = log_scores
+        self.res_pandas = pd.DataFrame(self._to_dict())
 
-if __name__ == "__main__":
-    bs = bayesSets()
-    bs.loadBinarizedDataFromJSON(jsonFile)
+    def _to_dict(self, maxItems=1000):
+        if maxItems is None:
+            maxItems = len(self.log_scores)
+        resDict = {
+                    'item_id': [self.item_index.keys()[r] for r in self.ranked_ids[:maxItems]],
+                    'item_desc': [self.item_index[r] for r in self.ranked_ids[:maxItems]],
+                    'log_score': [self.log_scores[r] for r in self.ranked_ids[:maxItems]]
+                    }
+        return resDict
+
+    def prettyPrint(self, maxItems = 50):
+        """
+        print the results
+        """
+
+        print "Your Query:"
+        print
+        for item in self.queryItemList:
+            print item
+        print " Bayesian Sets Algorithm recommends:"
+        #res = pd.DataFrame(self._to_dict())
+        print self.res_pandas.head(maxItems)
+
+    def dump(self):
+        pass
+
 
 
 
